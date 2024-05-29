eb7e74cd898b0ab74cb8cb1825b31dced0603468
zhanar2024 <160571337+zhanar2024@users.noreply.github.com>
2024-05-29 15:52:39 -0400

    added changes

--- alab308.4.1.js
+++ alab308.4.1.js
@@ -1,18 +1,38 @@
 const csv =
   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
-function parseCSV(csvString) {
+//Turning csv into an array of arrays
+
+function csvToArrayOfArrays(csvString) {
   const rows = csvString.split("\n");
+  const arrayOfArrays = [];
 
-  const result = [];
-
-  for (let i = 0; i < rows.length; i++) {
-    const columns = rows[i].split(",");
-    result.push(columns);
+  for (const row of rows) {
+    arrayOfArrays.push(row.split(","));
   }
+  return arrayOfArrays;
+}
 
+// Turning an array of arrays to array of objects
+
+function arrayOfArraysToObjects(arrayOfArrays) {
+  const headers = arrayOfArrays.shift();
+  const result = [];
+  for (let i = 1; i < arrayOfArrays.length; i++) {
+    const row = arrayOfArrays[i];
+    const newObject = {};
+    for (let j = 0; j < headers.length; i++) {
+      newObject[headers[j]] = row[j];
+    }
+    result.push(newObject);
+  }
   return result;
 }
 
-const parsedData = parseCSV(csv);
-console.log(parsedData);
+const arrayOfArrays = csvToArrayOfArrays(csv);
+const arrayOfObjects = arrayOfArraysToObjects(arrayOfArrays);
+
+//Checking if it worked
+console.log(csv);
+console.log(arrayOfArrays);
+console.log(arrayOfObjects);
